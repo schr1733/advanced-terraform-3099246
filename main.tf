b332393019ce3d4dd6a09c7f6eea98263503b68d
schr1733 <scott.christensen@concentrix.com>
2025-04-25 17:45:33 -0500

    .

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ### PROVIDER
 provider "google" {
-  project = "advancedterraform" #replace this with your project-id
+  project = "deft-ellipse-457216-g3" #replace this with your project-id
   region  = "us-central1"
   zone    = "us-central1-a"
 }
